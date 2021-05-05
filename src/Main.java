29e285f63d070ac9d598c5f6a67491ab08eff1af
Łukasz Rakowiecki <lukasz.er17@gmail.com>
2021-05-05 20:32:00 +0200

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import pl.blueenergy.document.DocumentDao;
+
+public class
+Main {
+	
+	public static void main(String[] args) {
+		DocumentDao documentDao = new DocumentDao();
+		ProgrammerService programmerService = new ProgrammerService();
+		programmerService.execute(documentDao);
+	}
+}
