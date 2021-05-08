d07233ca2e22289e0444d19192236af2c43bd269
lukasz rakowiecki <lukasz.er17@gmail..com>
2021-05-08 09:35:45 +0200

    exe 1

--- src/main/java/pl/blueenergy/service/ProgrammerService.java
+++ src/main/java/pl/blueenergy/service/ProgrammerService.java
@@ -5,15 +5,23 @@ import pl.blueenergy.document.Document;
 import pl.blueenergy.document.DocumentDao;
 import pl.blueenergy.document.Questionnaire;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ProgrammerService {
 
+
+	final DocumentDao documentDao = new DocumentDao();
+
 	public void execute(DocumentDao documentDao) {
 		List<Document> allDocumentsInDatabase = documentDao.getAllDocumentsInDatabase();
 
+		System.out.println(avgAnswersQuestionnaire());
+	}
 
+	private List<Document> getDocumentList(){
+		return documentDao.getAllDocumentsInDatabase();
 	}
 
 	private List<Questionnaire> getQuestionnaireList(List<Document> list){
@@ -28,5 +36,14 @@ public class ProgrammerService {
 				.filter(dok -> dok instanceof ApplicationForHolidays)
 				.map(dok -> (ApplicationForHolidays) dok)
 				.collect(Collectors.toList());
+	}
+
+	//1. Policz, ile średnio możliwych odpowiedzi zawierają wszystkie pytania we wszystkich kwestionariuszach w systemie.
+	private double avgAnswersQuestionnaire(){
+		long count = getQuestionnaireList(getDocumentList()).stream()
+				.map(Questionnaire::getQuestions)
+				.count();
+
+		return count;
 	}
 }
